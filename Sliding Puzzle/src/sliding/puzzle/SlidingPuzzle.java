b977692086a1daa2884f57c09e10444f902f7492
kmhasan <kmhasan@SEUPC0657>
2016-02-09 13:05:50 +0600

    Sliding Puzzle in class implementation

--- Sliding Puzzle/src/sliding/puzzle/SlidingPuzzle.java
+++ Sliding Puzzle/src/sliding/puzzle/SlidingPuzzle.java
@@ -5,24 +5,78 @@
  */
 package sliding.puzzle;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author kmhasan
  */
 public class SlidingPuzzle {
-
+    State initialState;
+    State goalState;
+    
+    public SlidingPuzzle(int initialBoard[][],
+            int goalBoard[][]) {
+        initialState = new State(initialBoard);
+        goalState = new State(goalBoard);
+    }
+    
+    private void printPath(State state) {
+        if (state.getParent() != null)
+            printPath(state.getParent());
+        System.out.println(state);
+    }
+    
+    public void doBFS() {
+        boolean isExplored[] = new boolean[987654322];
+        boolean goalFound = false;
+        State finalState = null;
+        Queue<State> queue = new LinkedList<>();
+        queue.add(initialState);
+        isExplored[initialState.mapToInteger()] = true;
+        
+        while (!queue.isEmpty() && !goalFound) {
+            State currentState = queue.poll();
+            if (currentState == null)
+                break;
+            for (Action action: Action.values()) {
+                State nextState = currentState.nextState(action);
+                if (nextState == null)
+                    continue;
+                if (isGoal(nextState)) {
+                    goalFound = true;
+                    finalState = nextState;
+                    break;
+                }
+                int map = nextState.mapToInteger();
+                if (!isExplored[map]) {
+                    queue.add(nextState);
+                    isExplored[map] = true;
+                }
+            }
+        }
+        if (goalFound) {
+            System.out.println("Found a solution!");
+            printPath(finalState);
+        } else {
+            System.out.println("No solution!");
+        }
+    }
+    
+    public boolean isGoal(State state) {
+        return state.equals(goalState);
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int board[][] = {{1, 2, 3}, {4, 5, 6}, {7, 9, 8}};
-        State state = new State(board);
-        System.out.println(state);
-        
-        for (Action action: Action.values()) {
-            System.out.println(action);
-            System.out.println(state.nextState(action));
-        }
+        int initialBoard[][] = {{1, 2, 3}, {4, 5, 6}, {9, 7, 8}};
+//        int initialBoard[][] = {{8, 6, 7}, {2, 5, 4}, {3, 9, 1}};
+        int goalBoard[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        SlidingPuzzle slidingPuzzle = new SlidingPuzzle(initialBoard, goalBoard);
+        slidingPuzzle.doBFS();
     }
     
 }
